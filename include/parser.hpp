469bc3f272b4d552e4fc529ebc6340f38f6cef3b
ericbarron329 <ericbarron329@gmail.com>
2025-01-18 23:40:38 -0800

    Create parser.hpp

--- include/parser.hpp
+++ include/parser.hpp
@@ -1,6 +1,27 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
 #include "lexer.hpp"
 #include "ast.hpp"
 
 class Parser {
-    
-}
+private:
+    Lexer lexer;
+    Token currentToken;
+
+    void advance();
+
+    ASTNode* parseStatement();
+    ASTNode* parseLetStatement();
+    ASTNode* parsePrintStatement();
+    ASTNode* parseExpression();
+    ASTNode* parseTerm();
+    ASTNode* parseFactor();
+    void expect(TokenType type);
+
+public:
+    Parser(Lexer lexer);
+    ASTNode* parseProgram();
+};
+
+#endif
